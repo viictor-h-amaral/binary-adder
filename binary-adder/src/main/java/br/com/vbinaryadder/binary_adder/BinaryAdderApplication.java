ff3991634fef6a6c13fdbcee604075eb0d37f29b
viictor-h-amaral <victorhamaralom@gmail.com>
2026-05-24 17:28:53 -0300

    WebApp para documentação de projeto de soma binária

---
+++ binary-adder/src/main/java/br/com/vbinaryadder/binary_adder/BinaryAdderApplication.java
@@ -0,0 +1,13 @@
+package br.com.vbinaryadder.binary_adder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinaryAdderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinaryAdderApplication.class, args);
+	}
+
+}
---
+++ binary-adder/src/test/java/br/com/vbinaryadder/binary_adder/BinaryAdderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vbinaryadder.binary_adder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinaryAdderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
